be351b42b5f35be24f9b906ad51a2a46d6e9ecf0
sundar <sundarsodadasi1996@gmail.com>
2025-02-06 15:27:18 +0530

    First commit

---
+++ Radhet-roboshop/create-ec2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-09c813fb71547fc4f 
+SECURITY_GROUP_ID=sg-07a1cb99873cf26ec
+DOMAIN_NAME=sadhusundar.xyz
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
+for i in "${NAMES[@]}"
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z0824164232GERUT9YSUC --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
+
+# imporvement
+# check instance is already created or not
+# update route53 record
